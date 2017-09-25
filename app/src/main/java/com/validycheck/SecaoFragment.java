e22a7929115e1536611e466fa5042e13a4c4a894
Leandro N. Anjos <lnanjos@outlook.com>
2017-09-24 22:11:25 -0300

    update secao fragment OnCreateView method and list_item to list

--- app/src/main/java/com/validycheck/SecaoFragment.java
+++ app/src/main/java/com/validycheck/SecaoFragment.java
@@ -32,8 +32,10 @@ public class SecaoFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_secao, container, false);
+        View rootView = inflater.inflate(R.layout.list, container,false);
 
 
+
+        return rootView;
     }
 }
