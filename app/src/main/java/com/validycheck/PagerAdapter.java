1c6cf03c985dca23758df28c8221a240a6483e46
Leandro N. Anjos <lnanjos@outlook.com>
2017-09-24 21:37:17 -0300

    Add page adapter and SecaoAdapter

---
+++ app/src/main/java/com/validycheck/PagerAdapter.java
@@ -0,0 +1,30 @@
+package com.validycheck;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Samsung on 24/09/2017.
+ */
+
+public class PagerAdapter extends FragmentPagerAdapter {
+
+    public PagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new SecaoFragment();
+            default: return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 5;
+    }
+}
