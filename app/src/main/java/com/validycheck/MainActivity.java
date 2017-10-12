084f9cc7869ff90966e0f04eb3b87f6d85e49a81
Leandro N. Anjos <lnanjos@outlook.com>
2017-10-12 19:27:01 -0300

    add ProdutoEditorFragment in Page Adapter

--- app/src/main/java/com/validycheck/MainActivity.java
+++ app/src/main/java/com/validycheck/MainActivity.java
@@ -1,10 +1,10 @@
 package com.validycheck;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 
 import com.validycheck.adapter.PagerAdapter;
@@ -17,21 +17,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Find the view pager that will allow the user to swipe between fragments
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
         // Create an adapter that knows which fragment should be shown on each page
         PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager());
 
         // Set the adapter onto the view pager
         viewPager.setAdapter(adapter);
-        // Setup FAB to open EditorActivity
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SecaoEditorActivity.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                if (viewPager.getCurrentItem() == PagerAdapter.SECAO_FRAG){
+                    Intent intent = new Intent(MainActivity.this, SecaoEditorActivity.class);
+                    startActivity(intent);
+                } else if (viewPager.getCurrentItem()==PagerAdapter.PRODUTO_FRAG){
+                    Intent intent = new Intent(MainActivity.this, ProdutoEditorActivity.class);
+                    startActivity(intent);
+                }
             }
         });
+
     }
 }
